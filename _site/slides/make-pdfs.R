09b274295ccb496e9a397492f6b66b375f2d2e6a
Ryan Safner <ryansafner@gmail.com>
2023-04-26 11:02:48 -0400

    4.2 slides

--- _site/slides/make-pdfs.R
+++ _site/slides/make-pdfs.R
@@ -1,14 +1,14 @@
 # knit Rmd slides to html
-rmarkdown::render("slides/4.1-slides.Rmd")
+rmarkdown::render("slides/4.2-slides.Rmd")
 
 # make-pdfs
-pagedown::chrome_print(input = "https://micros23.classes.ryansafner.com/slides/4.1-slides.html",
-                       output = "slides/4.1-slides.pdf",
+pagedown::chrome_print(input = "https://micros23.classes.ryansafner.com/slides/4.2-slides.html",
+                       output = "slides/4.2-slides.pdf",
                        timeout = 300)
 
 # create png of first slide
-pagedown::chrome_print(input = "https://micros23.classes.ryansafner.com/slides/4.1-slides.html",
-                       output = "slides/4.1-slides.png",
+pagedown::chrome_print(input = "https://micros23.classes.ryansafner.com/slides/4.2-slides.html",
+                       output = "slides/4.2-slides.png",
                        selector = "div.remark-slide",
                        format = "png",
                        timeout = 300)
